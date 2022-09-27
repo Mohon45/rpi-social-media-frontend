05a8d3521bd92c598d24bda882f01b8aebf355b3
Md: Mohon <mdmohon6145@gmail.com>
2022-09-28 01:46:53 +0600

    bug fixed

--- src/shared/Navbar/Navbar.js
+++ src/shared/Navbar/Navbar.js
@@ -14,46 +14,46 @@ const Navbar = () => {
           <a className="navbar-brand" href="#">
             CMT &nbsp; 19-20
           </a>
-          {user.email ? (
-            <div className="d-flex align-items-center">
-              <h4 className="me-3">{user.displayName}</h4>
-              <div className="dropdown profile-img me-3">
-                <span
-                  className="dropdown-toggle"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  <img
-                    src="https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/fox.jpg"
-                    width="60"
-                    height="60"
-                    className="rounded-circle border border-4 "
-                  />
-                </span>
+          <div className="d-flex align-items-center">
+            <h4 className="me-3">{user.displayName}</h4>
+            <div className="dropdown profile-img me-3">
+              <span
+                className="dropdown-toggle"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                <img
+                  src="https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/fox.jpg"
+                  width="60"
+                  height="60"
+                  className="rounded-circle border border-4 "
+                />
+              </span>
 
-                <ul className="dropdown-menu">
-                  <li>
-                    <Link className="dropdown-item" to="#">
-                      Profile
-                    </Link>
-                  </li>
-                  {user.email ? (
+              <ul className="dropdown-menu">
+                {user.email ? (
+                  <>
+                    <li>
+                      <Link className="dropdown-item" to="#">
+                        Profile
+                      </Link>
+                    </li>
                     <li>
                       <Link onClick={logout} className="dropdown-item" to="/">
                         Log out
                       </Link>
                     </li>
-                  ) : (
-                    <li>
-                      <Link className="dropdown-item" to="/login">
-                        Login
-                      </Link>
-                    </li>
-                  )}
-                </ul>
-              </div>
+                  </>
+                ) : (
+                  <li>
+                    <Link className="dropdown-item" to="/login">
+                      Login
+                    </Link>
+                  </li>
+                )}
+              </ul>
             </div>
-          ) : null}
+          </div>
         </div>
       </nav>
     </div>
